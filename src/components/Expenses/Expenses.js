85b1f9b9aca1464bb687341520ff64390058a0cc
Wazzupek01 <wazzupekgaming@gmail.com>
2022-06-26 20:56:31 +0200

    rendering dynamically items and filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,30 +12,26 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return filteredYear === expense.date.getFullYear().toString();
+  });
+
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={addExpensesFilterHandler} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={addExpensesFilterHandler}
         />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        ;
       </Card>
     </div>
   );
